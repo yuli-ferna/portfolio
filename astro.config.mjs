16628d5da6f51095f3713fd90f5bd21f793d2f15
yuli-ferna <yuliferna123@gmail.com>
2025-07-09 16:30:25 -0400

    fix base

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,7 +9,7 @@ import tailwindcss from '@tailwindcss/vite';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://yuli-ferna.github.io',
-  base: "/",
+  base: "portfolio",
   devToolbar: {
     enabled: false
   },
